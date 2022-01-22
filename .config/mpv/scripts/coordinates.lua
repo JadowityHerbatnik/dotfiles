d7986fbd5d774ac4168a93e9ee513ec8a296f426
JadowityHerbatnik <maciej.mancewicz@gmail.com>
2022-01-22 13:41:21 +0100

    Improve coordinates mpv script
      - show native video coordinates instead of scaled

--- .config/mpv/scripts/coordinates.lua
+++ .config/mpv/scripts/coordinates.lua
@@ -1,5 +1,12 @@
 mp.add_key_binding("MOUSE_BTN0", function()
-    local x, y = mp.get_mouse_pos()
     local time_pos = mp.get_property_number("time-pos")
-    mp.osd_message("x: " ..x.. " y: " ..y.. "\n" ..time_pos.. "", 1)
+
+    local mouse_x, mouse_y = mp.get_mouse_pos()
+    local osd_width, osd_height = mp.get_osd_size()
+    local video_width = mp.get_property("width")
+    local video_height = mp.get_property("height")
+    local x = ( mouse_x / osd_width ) * video_width
+    local y = ( mouse_y / osd_height ) * video_height
+
+    mp.osd_message("x: " ..x.. "\ny: " ..y.. "\ntime: " ..time_pos.. "s", 1)
 end)
